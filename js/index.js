3c688fa2cac933f777591a3455c3d46c3bf1a201
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-07-04 09:39:50 -0700

    added dark mode some sections of home

--- js/index.js
+++ js/index.js
@@ -1,28 +1,94 @@
-// let star = document.querySelectorAll('.star')
+const lable = document.querySelector('.navbar__menu-switch')
+const spanPlayer = document.querySelector('.navbar__menu-switchBtn')
+const HeroHead = document.querySelector('.heroTop')
+const popDiv = document.querySelector('.bgFf')
+const regDiv = document.querySelector('.bgFe')
+const cardsItem = document.querySelectorAll('.cards__item')
+const cardShish = document.querySelectorAll('.menuPack__dishCards-item')
+const titleH = document.querySelectorAll('h3, h4, h5, h6')
+const allButtons = document.querySelectorAll('button')
+const testimony = document.querySelector('.testimony')
+const testimonyCards = document.querySelectorAll('.flexBoxKrop__cards-item')
 
-// star.forEach((item) => {
+let themeMode = localStorage.getItem("theme");
 
-//     let liked = localStorage.getItem('_like')
+if (themeMode !== 'dark') {
+    HeroHead.classList.remove('CheangedHero')
+    spanPlayer.classList.remove('changePos')
+    popDiv.classList.remove('darkPop')
+    cardsArrayRe(cardsItem, cardShish, titleH, allButtons, testimonyCards)
+    regDiv.classList.remove('regDiv')
+    testimony.classList.remove('testimonyAg')
+} else {
+    HeroHead.classList.add('CheangedHero')
+    regDiv.classList.add('regDiv')
+    spanPlayer.classList.add('changePos')
+    popDiv.classList.add('darkPop')
+    testimony.classList.add('testimonyAg')
+    cardsArray(cardsItem, cardShish, titleH, allButtons, testimonyCards)
+}
 
-//     if (liked !== true) {
-//         item.classList.remove('fillColor')
-//     }else{
-//         item.classList.add('fillColor')
-//     }
+function cardsArray(param1, param2, param3, param4, param5) {
+    param1.forEach(item => {
+        item.classList.add('cardsItem')
+    })
 
-//     item.addEventListener('click', (e) => {
-//        let changedStar = e.target;
-        
-//        let liking = localStorage.getItem('_like')
+    param2.forEach(item => {
+        item.classList.add('darkCardShish')
+    })
 
-//        if(liking == true){
-//         changedStar.classList.remove('fillColor')
-//         localStorage.setItem('_like', JSON.stringify(false))
-//        }else{
-//         changedStar.classList.toggle('fillColor')
-//         localStorage.setItem('_like', JSON.stringify(true))
-//        }
+    param3.forEach(item => {
+        item.classList.add('darkTitle')
+    })
 
-//     })
-    
-// })
+    param4.forEach(item => {
+        item.classList.add("btnFontColor")
+    })
+
+    param5.forEach(item => {
+        item.classList.add('testimonyCard')
+    })
+}
+//functions for cards
+function cardsArrayRe(param1, param2, param3, param4, param5) {
+    param1.forEach(item => {
+        item.classList.remove('cardsItem')
+    })
+
+    param2.forEach(item => {
+        item.classList.remove('darkCardShish')
+    })
+
+    param3.forEach(item => {
+        item.classList.remove('darkTitle')
+    })
+
+    param4.forEach(item => {
+        item.classList.remove("btnFontColor")
+    })
+
+    param5.forEach(item => {
+        item.classList.remove('testimonyCard')
+    })
+}
+
+lable.addEventListener('click', () => {
+    if (themeMode === 'dark') {
+        popDiv.classList.remove('darkPop')
+        regDiv.classList.remove('regDiv')
+        HeroHead.classList.remove('CheangedHero')
+        spanPlayer.classList.remove('changePos')
+        localStorage.setItem('theme', 'light')
+        testimony.classList.remove('testimonyAg')
+        cardsArrayRe(cardsItem, cardShish, titleH, allButtons, testimonyCards)
+    } else {
+        popDiv.classList.add('darkPop')
+        regDiv.classList.add('regDiv')
+        HeroHead.classList.add('CheangedHero')
+        spanPlayer.classList.add('changePos')
+        testimony.classList.add('testimonyAg')
+        localStorage.setItem('theme', 'dark')
+        cardsArray(cardsItem, cardShish, titleH, allButtons, testimonyCards);
+    }
+})
+
